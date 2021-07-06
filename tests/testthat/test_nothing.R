eeb1e31086171bdbc8a5ed63cecfd036785c7c43
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-07-06 12:57:40 -0700

    🔃 Load package

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -1,7 +1,8 @@
 library(testthat)
+library(FeralCatEradication)
 
 test_that("all is ok", {
-  expected_values = 1
+  expected_value = 1
   obtained_value = return_one()
   expect_equal(expected_value, obtained_value)
 })
