b43ef03d45c6c36c251d9d93ff4f9385a554d606
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-07-16 11:37:24 -0700

    ✅ Assess some examples for the function

--- tests/testthat/test_feral_cat.R
+++ tests/testthat/test_feral_cat.R
@@ -7,9 +7,23 @@ test_that("all is ok", {
   expect_equal(expected_value, obtained_value)
 })
 
-test_that("Eigenvalor", {
-  expected_eigenvalue <- 3
-  matriz <- matrix(c(2, 1, 1, 2), nrow = 2)
-  obtained_eigenvalue <- max.lambda(matriz)
-  expect_equal(expected_eigenvalue, obtained_eigenvalue)
+describe("Get the first eigenvalue og the Leslie Matrix", {
+  it("Matrix 2x2 real eigenvalues", {
+    expected_eigenvalue <- 3
+    matriz <- matrix(c(2, 1, 1, 2), nrow = 2)
+    obtained_eigenvalue <- max.lambda(matriz)
+    expect_equal(expected_eigenvalue, obtained_eigenvalue)
+  })
+  it("Matrix 3x3 with complex eigenvalues", {
+    expected_eigenvalue <- -0.5
+    matriz <- matrix(c(0, 1, 0, 0, 0, 1, 1, 0, 0), nrow = 3)
+    obtained_eigenvalue <- max.lambda(matriz)
+    expect_equal(expected_eigenvalue, obtained_eigenvalue)
+  })
+  it("Diagonal matrix 3x3", {
+    expected_eigenvalue <- 3
+    matriz <- matrix(c(1, 0, 0, 0, 2, 0, 0, 0, 3), nrow = 3)
+    obtained_eigenvalue <- max.lambda(matriz)
+    expect_equal(expected_eigenvalue, obtained_eigenvalue)
+  })
 })
