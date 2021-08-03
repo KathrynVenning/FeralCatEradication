d78fe62d1a15637c0249851f1b5d75b8648c891c
maritza <maritza.bello@islas.org.mx>
2021-08-03 13:09:21 -0700

    ♻️ refactor code

--- FeralCatEradication/R/feral_cat.R
+++ FeralCatEradication/R/feral_cat.R
@@ -14,3 +14,35 @@ stable_stage_dist <- function(x) {
   parallel_matrix <- x %*% real_first_eigen_vector
   (parallel_matrix / (sum(parallel_matrix)))[, 1]
 }
+
+# Generation length function
+# reproductive value (r_0) where leslie_matrix = Leslie matrix; age_max = maximum age of females
+#' @export
+r_val <- function(leslie_matrix, age_max) {
+  # define the transition matrix
+  transition_matrix <- leslie_matrix[1:age_max, 1:age_max]
+  transition_matrix[1, 1:(age_max)] <- 0
+
+  # define the fertility matrix
+  fertility_matrix <- leslie_matrix[1:age_max, 1:age_max]
+  diag(fertility_matrix[2:age_max, 1:(age_max - 1)]) <- 0
+
+  # define the identity matrix
+  identity_matrix <- matrix(data = 0, nrow = age_max, ncol = age_max)
+  diag(identity_matrix) <- 1
+
+  # define the fundamental matrix
+  n_fund <- MASS::ginv(identity_matrix - transition_matrix)
+
+  # define the reproductive matrix
+  reproductive_matrix <- fertility_matrix %*% n_fund
+
+  # define r_0 (number of female offspring produced per female during lifetime)
+  r_0 <- Re((eigen(reproductive_matrix)$values)[1])
+
+  # output
+  print("number of female offspring produced per female during its lifetime")
+  print("_________________________________________________________________")
+  print(r_0)
+
+}
