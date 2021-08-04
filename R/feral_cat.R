04db54ee90681aa5c0380a4c86aa8e729573b7b6
Xavier Paredes <jxavierpp1@gmail.com>
2021-08-04 15:35:28 -0700

    ♻️🔥 Delete print in screen

--- R/feral_cat.R
+++ R/feral_cat.R
@@ -48,7 +48,5 @@ total_female_offspring_per_female <- function(leslie_matrix, age_max) {
 # where leslie_matrix is a Leslie Matrix
 g_val <- function(leslie_matrix, age_max) {
   mean_generation_time <- (log(total_female_offspring_per_female(leslie_matrix, age_max))) / (log(Re((eigen(leslie_matrix)$values)[1])))
-  print("mean generation time")
-  print("____________________")
-  print(mean_generation_time)
+  return(mean_generation_time)
 }
