ca8249a0dfaa3e586cb10eb1211a9fa1df968501
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-05 13:07:18 -0700

    🎨 Make format with styler

--- src/FeralCatEradication.R
+++ src/FeralCatEradication.R
@@ -14,7 +14,7 @@ options(scipen = 1000)
 # functions
 # beta distribution shape parameter estimator function
 est_beta_params <- function(mu, var) {
-  alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
+  alpha <- ((1 - mu) / var - 1 / mu) * mu^2
   beta <- alpha * (1 / mu - 1)
   return(params = list(alpha = alpha, beta = beta))
 }
@@ -42,8 +42,8 @@ m_sd_vec <- c(0.18 * m_vec[1], 0.18 * m_vec[2], a_m_sd, a_m_sd, a_m_sd, a_m_sd, 
 s_vec <- c(0.46, 0.46, 0.7, 0.7, 0.7, 0.7)
 
 # survival errors based on Budke & Slater
-y1_2_s_sd <- mean(c(((0.46 - 0.27) / 2), ((0.73 - 0.46) / 2))) #mean and standard deviations, juvenile survival
-a_s_sd <- mean(c(((0.7 - 0.55) / 2), ((0.78 - 0.7) / 2))) #mean and standard deviations, adult survival
+y1_2_s_sd <- mean(c(((0.46 - 0.27) / 2), ((0.73 - 0.46) / 2))) # mean and standard deviations, juvenile survival
+a_s_sd <- mean(c(((0.7 - 0.55) / 2), ((0.78 - 0.7) / 2))) # mean and standard deviations, adult survival
 # Mean and standard deviations vector, juvenile and adult survival:
 s_sd_vec <- c(y1_2_s_sd, y1_2_s_sd, a_s_sd, a_s_sd, a_s_sd, a_s_sd)
 
@@ -64,7 +64,7 @@ gen_l <- FeralCatEradication::g_val(popmat, age_max) # mean generation length
 # initial population vector
 pop_found <- 1629 # +/- 661 founding population size Hohnen et al 2020
 ssd <- FeralCatEradication::stable_stage_dist(popmat)
-init_vec <- ssd * pop_found #initial population vector
+init_vec <- ssd * pop_found # initial population vector
 
 # Project
 # set time limit for projection in 1-yr increments
@@ -72,15 +72,15 @@ yr_now <- 2020 # update if more data available post-2010
 #************************
 yr_end <- 2030 # set projection end date
 #************************
-t <- (yr_end - yr_now) #timeframe
+t <- (yr_end - yr_now) # timeframe
 
 tot_f <- sum(popmat_orig[1, ])
-popmat <- popmat_orig #resets matrix
-yr_vec <- seq(yr_now, yr_end) #year vector, 2020, 2021, 2022...
+popmat <- popmat_orig # resets matrix
+yr_vec <- seq(yr_now, yr_end) # year vector, 2020, 2021, 2022...
 
 # set population storage matrices
-n_mat <- matrix(0, nrow = age_max, ncol = (t + 1)) #empty matrix
-n_mat[, 1] <- init_vec #fill first matrix column with initial population vector
+n_mat <- matrix(0, nrow = age_max, ncol = (t + 1)) # empty matrix
+n_mat[, 1] <- init_vec # fill first matrix column with initial population vector
 
 # set up projection loop
 for (i in 1:t) {
@@ -97,9 +97,9 @@ plot(yrs, n_pred, type = "b", lty = 2, pch = 19, xlab = "year", ylab = "N")
 # Population rate of increase relative to carry capacity
 # Larger distance between populationa and K = faster population growth
 k_max <- 2 * pop_found
-k_min <- 1 #not used
-k_vec <- c(1, pop_found / 2, pop_found, 0.75 * k_max, k_max) #1= k_min, .75 = red_thresh??
-red_thresh <- 0.75 #not used
+k_min <- 1 # not used
+k_vec <- c(1, pop_found / 2, pop_found, 0.75 * k_max, k_max) # 1= k_min, .75 = red_thresh??
+red_thresh <- 0.75 # not used
 red_vec <- c(1, 0.965, 0.89, 0.79, 0.71)
 jpeg("reports/figures/k_vec.jpg")
 plot(k_vec, red_vec, pch = 19, type = "b")
@@ -108,18 +108,19 @@ k_red_dat <- data.frame(k_vec, red_vec)
 
 # logistic power function a/(1+(x/b)^c) #fits logistic power function to population relative to carry capacity, K
 param_init <- c(1, 15000, 2.5)
-fit_lp <- nls(red_vec ~ a / (1 + (k_vec / b) ^ c),
-              data = k_red_dat,
-              algorithm = "port",
-              start = c(a = param_init[1], b = param_init[2], c = param_init[3]),
-              trace = TRUE,
-              nls.control(maxiter = 1000, tol = 1e-05, minFactor = 1 / 1024))
+fit_lp <- nls(red_vec ~ a / (1 + (k_vec / b)^c),
+  data = k_red_dat,
+  algorithm = "port",
+  start = c(a = param_init[1], b = param_init[2], c = param_init[3]),
+  trace = TRUE,
+  nls.control(maxiter = 1000, tol = 1e-05, minFactor = 1 / 1024)
+)
 fit_lp_summ <- summary(fit_lp)
 jpeg("reports/figures/reduction_factor.jpg")
 plot(k_vec, red_vec, pch = 19, xlab = "N", ylab = "reduction factor")
 dev.off()
 k_vec_cont <- seq(1, 2 * pop_found, 1)
-pred_lp_fx <- coef(fit_lp)[1] / (1 + (k_vec_cont / coef(fit_lp)[2]) ^ coef(fit_lp)[3])
+pred_lp_fx <- coef(fit_lp)[1] / (1 + (k_vec_cont / coef(fit_lp)[2])^coef(fit_lp)[3])
 lines(k_vec_cont, pred_lp_fx, lty = 2, lwd = 3, col = "red")
 
 a_lp <- coef(fit_lp)[1]
@@ -139,7 +140,7 @@ popmat <- popmat_orig
 # set up projection loop
 for (i in 1:t) {
   tot_n_i <- sum(n_mat[, i])
-  pred_red <- a_lp / (1 + (tot_n_i / b_lp) ^ c_lp)
+  pred_red <- a_lp / (1 + (tot_n_i / b_lp)^c_lp)
   diag(popmat[2:age_max, ]) <- s_vec * pred_red
   popmat[age_max, age_max] <- 0
   n_mat[, i + 1] <- popmat %*% n_mat[, i]
@@ -149,7 +150,9 @@ n_pred <- colSums(n_mat)
 jpeg("reports/figures/something_with_Carry_capacity.jpg")
 # Untreated population increases, rate of increase relative to K, no stochastic sampling:
 plot(yrs, n_pred, type = "b", lty = 2, pch = 19, xlab = "year", ylab = "N", ylim = c(0, 1.05 * k_max))
-abline(h = k_max, lty = 2, col = "red") #carry capacity
-legend(yrs[2], n_pred[6], legend = c("N", "Carry capacity"),
-       col = c("black", "red"), lty = 1:2, cex = 0.8)
+abline(h = k_max, lty = 2, col = "red") # carry capacity
+legend(yrs[2], n_pred[6],
+  legend = c("N", "Carry capacity"),
+  col = c("black", "red"), lty = 1:2, cex = 0.8
+)
 dev.off()
--- tests/testthat/test_feral_cat.R
+++ tests/testthat/test_feral_cat.R
@@ -19,7 +19,7 @@ popmat[maximum_age, maximum_age] <- 0
 popmat[1, ] <- m_vec
 leslie_matrix_kathryn <- popmat
 
-leslie_matrix_gotelli <- matrix(c(1.5, 1.5, 0.25, 0, 0.8, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0.25, 0), nrow = 4, byrow=TRUE)
+leslie_matrix_gotelli <- matrix(c(1.5, 1.5, 0.25, 0, 0.8, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0.25, 0), nrow = 4, byrow = TRUE)
 
 describe("Get the first eigenvalue of the Leslie Matrix", {
   it("Matrix 2x2 real eigenvalues", {
@@ -43,12 +43,12 @@ describe("Get the first eigenvalue of the Leslie Matrix", {
   it("Kathryn example. Maximum age: 7 years; Matrix: 7x7", {
     expected_eigenvalue <- 1.25
     obtained_eigenvalue <- max_lambda(leslie_matrix_kathryn)
-    expect_equal(expected_eigenvalue, obtained_eigenvalue, tolerance=1e-3)
+    expect_equal(expected_eigenvalue, obtained_eigenvalue, tolerance = 1e-3)
   })
   it("Gotelli example. Maximum age: 4 years; Diagonal matrix: 4x4", {
     expected_eigenvalue <- 2.095
     obtained_eigenvalue <- max_lambda(leslie_matrix_gotelli)
-    expect_equal(expected_eigenvalue, obtained_eigenvalue, tolerance=1e-3)
+    expect_equal(expected_eigenvalue, obtained_eigenvalue, tolerance = 1e-3)
   })
 })
 
@@ -62,12 +62,12 @@ describe("Get the Malthusian parameter from the Leslie Matrix", {
   it("Kathryn example. Maximum age: 7 years; Matrix: 7x7", {
     expected_eigenvalue <- 0.223
     obtained_eigenvalue <- max_r(leslie_matrix_kathryn)
-    expect_equal(expected_eigenvalue, obtained_eigenvalue, tolerance=1e-3)
+    expect_equal(expected_eigenvalue, obtained_eigenvalue, tolerance = 1e-3)
   })
   it("Gotelli example. Maximum age: 4 years; Diagonal matrix: 4x4", {
     expected_eigenvalue <- 0.7397
     obtained_eigenvalue <- max_r(leslie_matrix_gotelli)
-    expect_equal(expected_eigenvalue, obtained_eigenvalue, tolerance=1e-3)
+    expect_equal(expected_eigenvalue, obtained_eigenvalue, tolerance = 1e-3)
   })
 })
 
@@ -76,17 +76,17 @@ describe("Get stable stage distribution", {
     expected_stable_stage_distribution <- c(0.22, 0.78)
     matriz <- matrix(c(0, 2, 0.3, 0.5), nrow = 2)
     obtained_stable_stage_distribution <- stable_stage_dist(matriz)
-    expect_equal(expected_stable_stage_distribution, obtained_stable_stage_distribution, tolerance=1e-3)
+    expect_equal(expected_stable_stage_distribution, obtained_stable_stage_distribution, tolerance = 1e-3)
   })
   it("Kathryn example. Maximum age: 7 years; Matrix: 7x7", {
     expected_stable_stage_distribution <- c(0.6026, 0.2219, 0.0817, 0.0458, 0.0256, 0.0144, 0.00733)
     obtained_stable_stage_distribution <- stable_stage_dist(leslie_matrix_kathryn)
-    expect_equal(expected_stable_stage_distribution, obtained_stable_stage_distribution, tolerance=1e-3)
+    expect_equal(expected_stable_stage_distribution, obtained_stable_stage_distribution, tolerance = 1e-3)
   })
   it("Gotelli example. Maximum age: 4 years; Diagonal matrix: 4x4", {
     expected_stable_stage_distribution <- c(0.67397, 0.25731, 0.06140, 0.00733)
     obtained_stable_stage_distribution <- stable_stage_dist(leslie_matrix_gotelli)
-    expect_equal(expected_stable_stage_distribution, obtained_stable_stage_distribution, tolerance=1e-3)
+    expect_equal(expected_stable_stage_distribution, obtained_stable_stage_distribution, tolerance = 1e-3)
   })
 })
 
@@ -95,20 +95,20 @@ describe("total_female_offspring_per_female", {
     expected_total_female_offspring_per_female <- c(1)
     maximum_age <- 3
     leslie_matrix <- matrix(c(1, 0, 0, 0, 2, 0, 0, 0, 3), nrow = 3)
-    obtained_total_female_offspring_per_female <- total_female_offspring_per_female(leslie_matrix,maximum_age)
-    expect_equal(expected_total_female_offspring_per_female, obtained_total_female_offspring_per_female, tolerance=1e-3)
+    obtained_total_female_offspring_per_female <- total_female_offspring_per_female(leslie_matrix, maximum_age)
+    expect_equal(expected_total_female_offspring_per_female, obtained_total_female_offspring_per_female, tolerance = 1e-3)
   })
   it("Kathryn example. Maximum age: 7 years; Matrix: 7x7", {
     expected_total_female_offspring_per_female <- c(2.0427)
     maximum_age <- 7
     obtained_total_female_offspring_per_female <- total_female_offspring_per_female(leslie_matrix_kathryn, maximum_age)
-    expect_equal(expected_total_female_offspring_per_female, obtained_total_female_offspring_per_female, tolerance=1e-3)
+    expect_equal(expected_total_female_offspring_per_female, obtained_total_female_offspring_per_female, tolerance = 1e-3)
   })
   it("Gotelli example. Maximum age: 4 years; Diagonal matrix: 4x4", {
     expected_total_female_offspring_per_female <- c(2.8)
     maximum_age <- 4
     obtained_total_female_offspring_per_female <- total_female_offspring_per_female(leslie_matrix_gotelli, maximum_age)
-    expect_equal(expected_total_female_offspring_per_female, obtained_total_female_offspring_per_female, tolerance=1e-3)
+    expect_equal(expected_total_female_offspring_per_female, obtained_total_female_offspring_per_female, tolerance = 1e-3)
   })
 })
 
@@ -117,19 +117,19 @@ describe("Mean generation time function", {
     expected_mean_generation <- c(0)
     maximum_age <- 3
     leslie_matrix <- matrix(c(1, 0, 0, 0, 2, 0, 0, 0, 3), nrow = 3)
-    obtained_mean_generation <- g_val(leslie_matrix,maximum_age)
-    expect_equal(expected_mean_generation, obtained_mean_generation, tolerance=1e-3)
+    obtained_mean_generation <- g_val(leslie_matrix, maximum_age)
+    expect_equal(expected_mean_generation, obtained_mean_generation, tolerance = 1e-3)
   })
   it("Kathryn example. Maximum age: 7 years; matrix: 7x7", {
     expected_mean_generation <- c(3.21)
     maximum_age <- 7
-    obtained_mean_generation <- g_val(leslie_matrix_kathryn,maximum_age)
-    expect_equal(expected_mean_generation, obtained_mean_generation, tolerance=1e-3)
+    obtained_mean_generation <- g_val(leslie_matrix_kathryn, maximum_age)
+    expect_equal(expected_mean_generation, obtained_mean_generation, tolerance = 1e-3)
   })
   it("Gotelli example. Maximum age: 4 years; Diagonal matrix: 4x4", {
     expected_mean_generation <- c(1.392)
     maximum_age <- 4
     obtained_mean_generation <- g_val(leslie_matrix_gotelli, maximum_age)
-    expect_equal(expected_mean_generation, obtained_mean_generation, tolerance=1e-3)
+    expect_equal(expected_mean_generation, obtained_mean_generation, tolerance = 1e-3)
   })
 })
