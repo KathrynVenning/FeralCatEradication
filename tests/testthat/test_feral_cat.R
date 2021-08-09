738cb80b5ab9df8f0c384fdc1a3aa8bea69846d5
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-09 12:32:39 -0700

    ✅🏷️Update version

--- tests/testthat/test_feral_cat.R
+++ tests/testthat/test_feral_cat.R
@@ -2,8 +2,8 @@ library(testthat)
 library(FeralCatEradication)
 
 describe("Get version of the module", {
-  it("The version is 0.1.3", {
-    expected_version <- c("0.1.3")
+  it("The version is 0.1.4", {
+    expected_version <- c("0.1.4")
     obtained_version <- packageVersion("FeralCatEradication")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
