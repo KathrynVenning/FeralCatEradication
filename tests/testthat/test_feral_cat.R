60ad54b7d449ec79cadbec3e31cb5d50514a55e5
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-08-10 15:57:05 -0700

    🔖 Update version number

--- tests/testthat/test_feral_cat.R
+++ tests/testthat/test_feral_cat.R
@@ -2,8 +2,8 @@ library(testthat)
 library(FeralCatEradication)
 
 describe("Get version of the module", {
-  it("The version is 0.1.4", {
-    expected_version <- c("0.1.4")
+  it("The version is 0.1.5", {
+    expected_version <- c("0.1.5")
     obtained_version <- packageVersion("FeralCatEradication")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
