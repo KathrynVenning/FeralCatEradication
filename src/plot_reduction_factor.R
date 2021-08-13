d60b020f3417011b40117554d4566f52028c40c3
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-13 09:11:32 -0700

    ⚡️👩🏿‍💻Use ggplot

--- src/plot_reduction_factor.R
+++ src/plot_reduction_factor.R
@@ -10,6 +10,8 @@ rm(list = ls())
 library(plotly)
 library(FeralCatEradication)
 options(scipen = 1000)
+library(ggplot2)
+
 
 # Compensatory density feedback
 # K = carry capacity
@@ -19,12 +21,10 @@ pop_found <- 1629
 k_max <- 2 * pop_found
 k_vec <- c(1, pop_found / 2, pop_found, 0.75 * k_max, k_max)
 red_vec <- c(1, 0.965, 0.89, 0.79, 0.71)
-plot(k_vec, red_vec, pch = 19)
-jpeg("reports/figures/k_vec.jpg")
-plot(k_vec, red_vec, pch = 19)
-dev.off()
 k_red_dat <- data.frame(k_vec, red_vec)
 
-jpeg("reports/figures/reduction_factor.jpg")
-plot(k_vec, red_vec, pch = 19, type = "b", xlab = "N", ylab = "reduction factor")
-dev.off()
+ggplot(k_red_dat, aes(k_vec, red_vec)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "N", y = "Reduction factor")
+ggsave("reports/figures/reduction_factor.jpg")
