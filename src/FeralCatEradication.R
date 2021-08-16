8f6986305d429493e01e4d94d63cf356ddd382ed
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-16 15:34:25 -0700

    ♻️ Refactor the function matrix_leslie

--- src/FeralCatEradication.R
+++ src/FeralCatEradication.R
@@ -12,47 +12,31 @@ library(FeralCatEradication)
 options(scipen = 1000)
 library(tidyverse)
 
-# functions
-# beta distribution shape parameter estimator function
-est_beta_params <- function(mu, var) {
-  alpha <- ((1 - mu) / var - 1 / mu) * mu^2
-  beta <- alpha * (1 / mu - 1)
-  return(params = list(alpha = alpha, beta = beta))
-}
-
 # create Leslie matrix
-age_max <- 7
 
 # Create vectors
 # Fertility
 # KI cat birth rates matrix, data for female offsping produced each year.
 # Data from Budke, C & Slater, M (2009)
 fertility <- c((0.745 / 3), 0.745, 2.52, 2.52, 2.52, 2.52, 1.98)
+age_max <- length(fertility)
 
 # Fertility errors based on Budke & Slater
 # Mean and standard deviations, juvenile fertility:
-juv_m_sd <- mean(c(((0.745 / 3 - 0.352 / 3) / 2), ((1.58 / 3 - 0.745 / 3) / 2)))
-fy_m_sd <- mean(c(((0.745 - 0.352) / 2), ((1.58 - 0.745) / 2))) # Mean and standard deviations, juvenile fertility
-a_m_sd <- mean(c(((2.52 - 1.98) / 2), ((3.78 - 2.52) / 2))) # Mean and standard deviations, adult fertility
-# Mean and standard deviations vector, juvenile and adult fertility:
-m_sd_vec <- c(0.18 * fertility[1], 0.18 * fertility[2], a_m_sd, a_m_sd, a_m_sd, a_m_sd, a_m_sd)
-
 # Survival
 # KI cat survival
 # probability of surviving from one year to the next. e.g surviving fourth year of life
 survival_probability <- c(0.46, 0.46, 0.7, 0.7, 0.7, 0.7)
-
-# survival errors based on Budke & Slater
-y1_2_s_sd <- mean(c(((0.46 - 0.27) / 2), ((0.73 - 0.46) / 2))) # mean and standard deviations, juvenile survival
-a_s_sd <- mean(c(((0.7 - 0.55) / 2), ((0.78 - 0.7) / 2))) # mean and standard deviations, adult survival
-# Mean and standard deviations vector, juvenile and adult survival:
-s_sd_vec <- c(y1_2_s_sd, y1_2_s_sd, a_s_sd, a_s_sd, a_s_sd, a_s_sd)
-
 # create matrix
-popmat <- matrix(data = 0, nrow = age_max, ncol = age_max)
-diag(popmat[2:age_max, ]) <- survival_probability
-popmat[age_max, age_max] <- 0
-popmat[1, ] <- fertility
+matrix_leslie <- function(fertility, survival_probability){
+  age_max <- length(fertility)
+  popmat <- matrix(data = 0, nrow = age_max, ncol = age_max)
+  diag(popmat[2:age_max, ]) <- survival_probability
+  popmat[age_max, age_max] <- 0
+  popmat[1, ] <- fertility
+  return(popmat)
+}
+popmat <- matrix_leslie(fertility, survival_probability)
 popmat_orig <- popmat # save original matrix
 
 # matrix properties
