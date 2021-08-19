100833abc9c24e182489dcdc6872ad3c60446344
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-19 13:13:30 -0700

    🔥 Delete unused variables

--- src/FeralCatEradication.R
+++ src/FeralCatEradication.R
@@ -9,42 +9,16 @@ library(FeralCatEradication)
 options(scipen = 1000)
 library(tidyverse)
 
-# create Leslie matrix
-
-# Create vectors
-# Fertility
-# KI cat birth rates matrix, data for female offsping produced each year.
-# Data from Budke, C & Slater, M (2009)
 fertility <- c((0.745 / 3), 0.745, 2.52, 2.52, 2.52, 2.52, 1.98)
-age_max <- length(fertility)
-
-# Fertility errors based on Budke & Slater
-# Mean and standard deviations, juvenile fertility:
-# Survival
-# KI cat survival
-# probability of surviving from one year to the next. e.g surviving fourth year of life
 survival_probability <- c(0.46, 0.46, 0.7, 0.7, 0.7, 0.7)
-# create matrix
-popmat <- FeralCatEradication::matrix_leslie(fertility, survival_probability)
-
-# matrix properties
-gen_l <- FeralCatEradication::g_val(popmat, age_max) # mean generation length
-
-# initial population vector
-pop_found <- 1629 # +/- 661 founding population size Hohnen et al 2020
-ssd <- FeralCatEradication::stable_stage_dist(popmat)
-init_vec <- ssd * pop_found # initial population vector
+initial_population <- 1629
 
 # Project
-# set time limit for projection in 1-yr increments
 yr_now <- 2020 # update if more data available post-2010
-#************************
 yr_end <- 2030 # set projection end date
-#************************
 
-# set population storage matrices
 population_with_cc <- Population$new(fertility, survival_probability)
-population_with_cc$run_generations(yr_now, yr_end, initial_population = init_vec)
+population_with_cc$run_generations(yr_now, yr_end, initial_population = initial_population)
 
 plotter <- Plotter_Population$new()
 plotter$plot(population_with_cc)
@@ -61,7 +35,7 @@ coefficients <- coefficients_proportion_realized_survival(k_vec, red_vec)
 
 # compensatory density-feedback deterministic model
 # set population storage matrices
-population_with_cc$run_generations(yr_now, yr_end, initial_population = init_vec, coefficients)
+population_with_cc$run_generations(yr_now, yr_end, initial_population = initial_population, coefficients)
 
 plotter$plot(population_with_cc)
 plotter$save("reports/figures/time_serie_individuals_with_carry_capacity.jpg")
