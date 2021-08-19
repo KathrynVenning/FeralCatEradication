8c9d88a52c830c9350746b028d4da90da74bb8fe
Xavier Paredes <jxavierpp1@gmail.com>
2021-08-19 11:43:04 -0700

    💫🏷️ Update tag

--- tests/testthat/test_feral_cat.R
+++ tests/testthat/test_feral_cat.R
@@ -2,8 +2,8 @@ library(testthat)
 library(FeralCatEradication)
 
 describe("Get version of the module", {
-  it("The version is 0.1.7", {
-    expected_version <- c("0.1.7")
+  it("The version is 0.1.8", {
+    expected_version <- c("0.1.8")
     obtained_version <- packageVersion("FeralCatEradication")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
