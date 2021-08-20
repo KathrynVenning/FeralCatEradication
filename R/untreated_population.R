a92aa6b9b6706dfca3244bd336981192227380dd
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-20 14:08:19 -0700

    ♻️ Refactor function get_stochastic_fertility

--- R/untreated_population.R
+++ R/untreated_population.R
@@ -4,3 +4,15 @@ est_beta_params <- function(mu, var) {
   beta <- alpha * (1 / mu - 1)
   return(params = list(alpha = alpha, beta = beta))
 }
+
+get_stochastic_fertility <- function(fertility, sd_fertility) {
+  fert.stch <- rnorm(length(fertility), fertility, sd_fertility)
+  fert.stoch <- ifelse(fert.stch < 0, 0, fert.stch)
+  return(fert.stoch)
+}
+
+get_stochastic_survival <- function(survival, sd_survival) {
+  parameter <- estBetaParams(survival, sd_survival^2)
+  s.stoch <- rbeta(length(survival), parameter[["alpha"]], parameter[["beta"]])
+  return(s.stoch)
+}
--- tests/testthat/test_untreated_population.R
+++ tests/testthat/test_untreated_population.R
@@ -17,3 +17,15 @@ describe(" The function est_beta_params", {
     expect_equal(expected, obtained)
   })
 })
+
+describe("get_stochastic_fertility", {
+  it("limits are correts", {
+    fertility <- c(3,4)
+    sd_fertility <- c(0.1, 0.2)
+    obtained_stochastic_fertility <- get_stochastic_fertility(fertility, sd_fertility)
+    all_less_than_five <- all(obtained_stochastic_fertility < 5)
+    expect_true(all_less_than_five)
+    all_greater_than_two <- all(obtained_stochastic_fertility > 2)
+    expect_true(all_greater_than_two)
+  })
+})
