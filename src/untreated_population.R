05b5ea8e7040c53e700df3f07e2315cfe48ac900
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-25 08:55:35 -0700

    🚨🐍 Change to snake case

--- src/untreated_population.R
+++ src/untreated_population.R
@@ -20,20 +20,20 @@ survival$set_standard_desviations(std_fertility, std_survival_probability)
 yr_now <- 2020 # update if more data available post-2010
 yr_end <- 2030 # set projection end date
 number_year <- yr_end - yr_now + 1
-n.sums.mat <- matrix(data = 0, nrow = iter, ncol = number_year)
+n_sums_mat <- matrix(data = 0, nrow = iter, ncol = number_year)
 population <- Population$new(survival)
 for (simulation in seq(1, iter)) {
   population$run_generations(yr_now, yr_end, initial_population = initial_population, coefficients)
-  n.sums.mat[simulation, ] <- colSums(population$n_mat) / initial_population
+  n_sums_mat[simulation, ] <- colSums(population$n_mat) / initial_population
 }
 
 yrs <- seq(yr_now, yr_end)
-n.md <- apply(n.sums.mat, MARGIN = 2, median, na.rm = T) # mean over all iterations
-n.up <- apply(n.sums.mat, MARGIN = 2, quantile, probs = 0.975, na.rm = T) # upper over all iterations
-n.lo <- apply(n.sums.mat, MARGIN = 2, quantile, probs = 0.025, na.rm = T) # lower over all iterations
-untreated <- data.frame(yrs, n.md, n.lo, n.up)
+n_md <- apply(n_sums_mat, MARGIN = 2, median, na.rm = T) # mean over all iterations
+n_up <- apply(n_sums_mat, MARGIN = 2, quantile, probs = 0.975, na.rm = T) # upper over all iterations
+n_lo <- apply(n_sums_mat, MARGIN = 2, quantile, probs = 0.025, na.rm = T) # lower over all iterations
+untreated <- data.frame(yrs, n_md, n_lo, n_up)
 
-ggplot(untreated, aes(x = yrs, y = n.md)) +
+ggplot(untreated, aes(x = yrs, y = n_md)) +
   geom_line(colour = "blue") +
-  geom_ribbon(aes(ymin = n.lo, ymax = n.up), alpha = 0.2)
+  geom_ribbon(aes(ymin = n_lo, ymax = n_up), alpha = 0.2)
 ggsave("simulation.jpg")
