9731495afb0270a03d19d1240f4f641787fa22ab
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-08-31 12:41:35 -0700

    ☂️ Add file to do coverage

---
+++ tests/testthat/coverage.R
@@ -0,0 +1,16 @@
+library(covr)
+library(testthat)
+cobertura <- file_coverage(
+  c(
+    "R/feral_cat.R",
+    "R/monthly_matrix_leslie.R",
+    "R/untreated_population.R"
+  ),
+  c(
+    "tests/testthat/test_feral_cat.R",
+    "tests/testthat/test_monthly_matrix_leslie.R",
+    "tests/testthat/test_plots.R",
+    "tests/testthat/test_untreated_population.R"
+  )
+)
+covr::codecov(covertura=cobertura, token='d40cba41-8ee3-414d-9e04-581d33a42b62')
