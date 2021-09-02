e38b8185e494198e3c650e748b2f7cf56834d96e
maritza <maritza.bello@islas.org.mx>
2021-09-02 12:35:52 -0700

    ⚡️ Add new class population with CC and harvest

--- src/new_plot.R
+++ src/new_plot.R
@@ -4,93 +4,35 @@ harv.prop.consist <- seq(0.2, 0.99, 0.05) # sequence harvest/culling quotas, e.g
 # define our quasi-extinction probability storage vector
 min.med.n <- min.lo.n <- min.up.n <- rep(0, length(harv.prop.consist))
 
-for (s in 1:length(harv.prop.consist)) {
+library("ggplot2")
+library(FeralCatEradication)
+source("R/feral_cat.R")
+source("src/parameters_of_fertility_and_survival.R")
+####################################################
+## iterations and quasi ext for each following model
+####################################################
+iter <- 10000 # final model run at 10 000
+itdiv <- iter / 1000 # final model rate at iter/1000
+################################################################################################################
+## untreated population
+###############################################################################################################
+## stochatic projection with density feedback
+## set storage matrices & vectors
 
-  # set storage matrices & vectors
-  n.sums.mat <- matrix(data = 0, nrow = iter, ncol = (t + 1))
+initial_population <- 1629
+capacity <- Carry_Capacity$new()
+coefficients <- capacity$coefficients_model(half_capacity = initial_population)
+survival <- Stochastic_Survival_Fertility$new(fertility, survival_probability)
+survival$set_standard_desviations(std_fertility, std_survival_probability)
+yr_now <- 2020 # update if more data available post-2010
+yr_end <- 2030 # set projection end date
+interval_time <- Interval_Time$new(initial_year = yr_now, final_year = yr_end)
+number_year <- yr_end - yr_now + 1
+n_sums_mat <- matrix(data = 0, nrow = iter, ncol = number_year)
+population <- Population$new(survival)
+simulator2 <- Runner_Population_With_CC_harvest$new(population, coefficients, harv.prop.consist[4])
 
-  for (e in 1:iter) {
-    popmat <- popmat.orig
-
-    n.mat <- matrix(0, nrow = age.max, ncol = (t + 1))
-    n.mat[, 1] <- init.vec
-
-    for (i in 1:t) {
-      # stochastic survival values
-      s.alpha <- estBetaParams(s.vec, s.sd.vec^2)$alpha
-      s.beta <- estBetaParams(s.vec, s.sd.vec^2)$beta
-      s.stoch <- rbeta(length(s.alpha), s.alpha, s.beta)
-
-      # stochastic fertilty sampler (gaussian)
-      fert.stch <- rnorm(length(popmat[, 1]), popmat[1, ], m.sd.vec)
-      fert.stoch <- ifelse(fert.stch < 0, 0, fert.stch)
-
-      totN.i <- sum(n.mat[, i])
-      pred.red <- a.lp / (1 + (totN.i / b.lp)^c.lp)
-
-      popmat[1, ] <- fert.stoch
-      diag(popmat[2:age.max, ]) <- s.stoch * pred.red
-      # popmat[age.max,age.max] <- 0
-
-      n.mat[, i + 1] <- popmat %*% n.mat[, i]
-
-      ## harvest things here
-      n.mat[, i + 1] <- n.mat[, i + 1] - round(stable.stage.dist(popmat) * round(sum(n.mat[, i + 1]) * harv.prop.consist[s], 0), 0)
-
-
-      if (length(which(n.mat[, i + 1] < 0)) > 0) {
-        n.mat[which(n.mat[, i + 1] < 0), i + 1] <- 0
-      }
-    } # end i loop
-
-    n.sums.mat[e, ] <- as.vector((colSums(n.mat)) / pop.found) # / pop.mat for min proportion remaining population
-
-    if (e %% itdiv == 0) print(e)
-  } # end e loop
-
-  # calculate minimum population size
-
-  min.pop.vec <- apply(n.sums.mat, MARGIN = 1, min)
-  min.med.n[s] <- median(min.pop.vec, na.rm = T)
-  min.lo.n[s] <- quantile(min.pop.vec, probs = 0.025, na.rm = T)
-  min.up.n[s] <- quantile(min.pop.vec, probs = 0.975, na.rm = T)
-
-  n.md <- apply((n.sums.mat), MARGIN = 2, mean, na.rm = T) # minimum over all iterations
-  n.up <- apply((n.sums.mat), MARGIN = 2, quantile, probs = 0.975, na.rm = T) # upper over all iterations
-  n.lo <- apply((n.sums.mat), MARGIN = 2, quantile, probs = 0.025, na.rm = T) # lower over all iterations
-
-  plot(yrs, n.md, type = "l", xlab = "year", ylab = "minimum N", lwd = 2, ylim = c(0.95 * min(n.lo), 1.05 * max(n.up)))
-  lines(yrs, n.lo, lty = 2, col = "red", lwd = 1.5)
-  lines(yrs, n.up, lty = 2, col = "red", lwd = 1.5)
-
-  print("##############")
-  print(paste("harvest proportion = ", harv.prop.consist[s], sep = ""))
-  print("##############")
-} # ends S loop
-
-plot(harv.prop.consist, min.med.n, type = "l", pch = 19, xlab = "harvest proportion", ylab = "min N", ylim = c(min(min.lo.n), max(min.up.n)))
-lines(harv.prop.consist, min.lo.n, col = "red", lty = 2)
-lines(harv.prop.consist, min.up.n, col = "red", lty = 2)
-
-minn.prop.pop <- data.frame(harv.prop.consist, min.med.n, min.lo.n, min.up.n)
-
-
-## Inicializa parametros
-inicializaVariables()
-creaObjeto()
-## El tratamiento
-
-for (s in 1:length(harv.prop.consist)) {
-
-  # set storage matrices & vectors
-  n.sums.mat <- matrix(data = 0, nrow = iter, ncol = (t + 1))
-    n.sums.mat[e, ] <- as.vector((colSums(n.mat)) / pop.found) # / pop.mat for min proportion remaining population
-    simulator <- Runner_Population$new(s, coefficients)
-    for (simulation in seq(1, iter)) {
-      simulator$run_generations(interval_time, initial_population = initial_population)
-      n_sums_mat[simulation, ] <- colSums(population$n_mat) / initial_population
-    }
-    if (e %% itdiv == 0) print(e)
-  } # end e loop
-
-## Graficacion
+for (simulation in seq(1, iter)) {
+  simulator2$run_generations(interval_time, initial_population = initial_population)
+  n_sums_mat[simulation, ] <- colSums(simulator2$n_mat) / initial_population
+}
