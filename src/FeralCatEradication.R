9b81c9060c7fd1871f8952e7edd35c70ed779fcd
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-02 12:10:38 -0700

    ⚡️ Use new class

--- src/FeralCatEradication.R
+++ src/FeralCatEradication.R
@@ -6,6 +6,7 @@
 # libraries
 library(plotly)
 library(FeralCatEradication)
+source("R/feral_cat.R")
 options(scipen = 1000)
 library(tidyverse)
 
@@ -19,10 +20,11 @@ yr_end <- 2030 # set projection end date
 interval_time <- Interval_Time$new(initial_year = yr_now, final_year = yr_end)
 survival <- Survival_Fertility$new(fertility, survival_probability)
 population_with_cc <- Population$new(survival)
-population_with_cc$run_generations(interval_time, initial_population = initial_population)
+simulator <- Runner_Population$new(population_with_cc)
+simulator$run_generations(interval_time, initial_population = initial_population)
 
 plotter <- Plotter_Population$new()
-plotter$plot(population_with_cc)
+plotter$plot(simulator)
 plotter$save("reports/figures/time_serie_individuals.jpg")
 
 # Compensatory density feedback
@@ -33,8 +35,9 @@ capacity <- Carry_Capacity$new()
 coefficients <- capacity$coefficients_model(half_capacity = initial_population)
 # compensatory density-feedback deterministic model
 # set population storage matrices
-population_with_cc$run_generations(interval_time, initial_population = initial_population, coefficients)
+simulator2 <- Runner_Population_With_CC$new(population_with_cc, coefficients)
+simulator2$run_generations(interval_time, initial_population = initial_population)
 
-plotter$plot(population_with_cc)
+plotter$plot(simulator2)
 plotter$plot_carry_capacity(capacity)
 plotter$save("reports/figures/time_serie_individuals_with_carry_capacity.jpg")
