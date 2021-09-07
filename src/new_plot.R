92e6436dd2ab30c75feef39025b819ef792ea46f
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-09-07 11:10:59 -0700

    🎨 Make format with styler

--- src/new_plot.R
+++ src/new_plot.R
@@ -33,34 +33,33 @@ population <- Population$new(survival)
 
 yrs <- interval_time$get_time_sequence()
 for (s in 1:length(harv.prop.consist)) {
-  
+
   # set storage matrices & vectors
   simulator2 <- Runner_Population_With_CC_harvest$new(population, coefficients, harv.prop.consist[s])
   for (simulation in seq(1, iter)) {
     simulator2$run_generations(interval_time, initial_population = initial_population)
     n_sums_mat[simulation, ] <- colSums(simulator2$n_mat) / initial_population
   }
-  min.pop.vec <- apply(n_sums_mat, MARGIN=1, min)
-  min.med.n[s] <- median(min.pop.vec, na.rm=T)
-  min.lo.n[s] <- quantile(min.pop.vec, probs=0.025, na.rm=T)
-  min.up.n[s] <- quantile(min.pop.vec, probs=0.975, na.rm=T)
-  
-  n.md <- apply((n_sums_mat), MARGIN=2, mean, na.rm=T) # minimum over all iterations
-  n.up <- apply((n_sums_mat), MARGIN=2, quantile, probs=0.975, na.rm=T) # upper over all iterations
-  n.lo <- apply((n_sums_mat), MARGIN=2, quantile, probs=0.025, na.rm=T) # lower over all iterations
-  
-  plot(yrs,n.md,type="l",xlab="year", ylab="minimum N", lwd=2, ylim=c(0.95*min(n.lo),1.05*max(n.up)))
-  lines(yrs,n.lo,lty=2,col="red",lwd=1.5)
-  lines(yrs,n.up,lty=2,col="red",lwd=1.5)
-  
+  min.pop.vec <- apply(n_sums_mat, MARGIN = 1, min)
+  min.med.n[s] <- median(min.pop.vec, na.rm = T)
+  min.lo.n[s] <- quantile(min.pop.vec, probs = 0.025, na.rm = T)
+  min.up.n[s] <- quantile(min.pop.vec, probs = 0.975, na.rm = T)
+
+  n.md <- apply((n_sums_mat), MARGIN = 2, mean, na.rm = T) # minimum over all iterations
+  n.up <- apply((n_sums_mat), MARGIN = 2, quantile, probs = 0.975, na.rm = T) # upper over all iterations
+  n.lo <- apply((n_sums_mat), MARGIN = 2, quantile, probs = 0.025, na.rm = T) # lower over all iterations
+
+  plot(yrs, n.md, type = "l", xlab = "year", ylab = "minimum N", lwd = 2, ylim = c(0.95 * min(n.lo), 1.05 * max(n.up)))
+  lines(yrs, n.lo, lty = 2, col = "red", lwd = 1.5)
+  lines(yrs, n.up, lty = 2, col = "red", lwd = 1.5)
+
   print("##############")
-  print(paste("harvest proportion = ", harv.prop.consist[s], sep=""))
+  print(paste("harvest proportion = ", harv.prop.consist[s], sep = ""))
   print("##############")
-  
 } # ends S loop
 
-plot(harv.prop.consist, min.med.n, type="l", pch=19, xlab="harvest proportion", ylab="min N", ylim=c(min(min.lo.n),max(min.up.n)))
-lines(harv.prop.consist, min.lo.n, col="red", lty=2)
-lines(harv.prop.consist, min.up.n, col="red", lty=2)
+plot(harv.prop.consist, min.med.n, type = "l", pch = 19, xlab = "harvest proportion", ylab = "min N", ylim = c(min(min.lo.n), max(min.up.n)))
+lines(harv.prop.consist, min.lo.n, col = "red", lty = 2)
+lines(harv.prop.consist, min.up.n, col = "red", lty = 2)
 
 minn.prop.pop <- data.frame(harv.prop.consist, min.med.n, min.lo.n, min.up.n)
